dc36c394d80d9185bbdd66eb800665aefc01484a
vsumahrishi <vs.umahrishi@gmail.com>
2026-08-29 17:05:58 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'snapback-cache-v150';
+const CACHE_NAME = 'snapback-cache-v160';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
